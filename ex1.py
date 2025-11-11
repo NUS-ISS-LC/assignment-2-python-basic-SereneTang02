0dd84c641c5a28b161a866c4f7db359a739c0fb6
SereneTang <021130>
2025-11-11 23:01:22 +0800

    ass2

--- ex1.py
+++ ex1.py
@@ -1,3 +1,9 @@
 def find(s, n):
-# write your implementation here
+    index_map = {}
+    for i, x in enumerate(s):
+        complement = n - x
+        if complement in index_map:
+            return [index_map[complement], i]
+        index_map[x] = i
+    return []
     return None
